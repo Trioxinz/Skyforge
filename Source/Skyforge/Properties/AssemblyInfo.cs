2ba0ad0396ebf247b9c28a7f641742e3f10c1f1e
Trioxinz <Trioxinz@users.noreply.github.com>
2019-08-14 00:13:13 -0400

    Major Encryption Change
    Pulled the previous RSA TriNetwork key pair, has been replaced with the new and current RSA 8192 TriByte Developments key pair.

--- Source/Skyforge/Properties/AssemblyInfo.cs
+++ Source/Skyforge/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -8,9 +9,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Skyforge")]
 [assembly: AssemblyDescription("A Skyrim-NX-Toolkit GUI")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("TriNetwork")]
+[assembly: AssemblyCompany("TriByte Developments")]
 [assembly: AssemblyProduct("Skyforge")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.4.0")]
-[assembly: AssemblyFileVersion("0.0.4.0")]
+[assembly: AssemblyVersion("0.0.4.1")]
+[assembly: AssemblyFileVersion("0.0.4.1")]
+[assembly: NeutralResourcesLanguage("en")]
+
